3531391b09f94249e98cf3104b0fd775df857cf5
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-01-11 12:52:25 -0500

    added favicon

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+// config 
 const express = require("express");
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+const path = require('path')
+const favicon = require('serve-favicon')
 
 app.use(express.static("public")); // static content 
 
 // Middleware to parse body JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// favicon middleware 
+app.use(favicon(path.join(__dirname, 'public/assets/images', 'favicon.ico')))
+console.log((path.join(__dirname, 'public/assets/images', 'favicon.ico')))
+
+
 
 // Set Handlebars.
 var handlebars = require("express-handlebars");
